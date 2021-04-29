667b7193d7d08c7ee9c6ad2886c97cdff81ce998
Sipkó Miklós <miklossipko@gmail.com>
2021-04-29 10:40:04 +0200

    4 tests passed 1 test failed

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,25 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        } else {
+            try {
+                PrintWriter pw = new PrintWriter(output);
+                StringBuilder sb = new StringBuilder();
+                contacts.forEach((key, value) -> sb.append(key + ": " + value + System.lineSeparator()));
+                pw.write(sb.toString());
+                pw.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
